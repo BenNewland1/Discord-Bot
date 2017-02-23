ff9e22a1ee70ac7f52e851d2886ad880efdbb251
CinnaMonMon <ben.newland1998@gmail.com>
2017-02-22 16:40:17 -0800

    button 3 test

--- MingLee/Form1.cs
+++ MingLee/Form1.cs
@@ -21,5 +21,20 @@ namespace MingLee
         {
 
         }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
